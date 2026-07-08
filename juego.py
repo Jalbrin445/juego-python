8c49df4bcebaf458c3d6ff196dbca3e84b4e326e
Jalbrin445 <mezaguzmanjuanalbrin@gmail.com>
2026-07-08 17:19:30 -0500

    feat: Agregar clase CofreJAMG para definir tipos de cofres y su lógica de puntuación

--- juego.py
+++ juego.py
@@ -33,3 +33,59 @@ class ContrasenaIncorrectaErrorJAMG(Exception):
         self.mensajeJAMG = mensajeJAMG
         super().__init__(self.mensajeJAMG)
 
+## Sección de clases principales
+
+# Clase Cofre
+
+class CofreJAMG:
+
+    """
+    Docstring: clase encargada para definir los tipos de cofres 
+    según la contraseña y la aletoriedad:
+    """
+    TIPOS_COFRE_POSITIVO_JAMG = {
+        "Común": 10,
+        "Raro": 25,
+        "Legendario": 50
+    }
+    def __init__(self, es_validoJAMG=True):
+
+        if es_validoJAMG:
+
+            self.tipoJAMG = random.choice(list(self.TIPOS_COFRE_POSITIVO_JAMG.keys())) # Se selecciona el cofre de forma aleatoria, seleccionando solo las llaves, es decir, común, raro y legendario
+            self.puntosJAMG = self.TIPOS_COFRE_POSITIVO_JAMG[self.tipoJAMG] # aquí se selecciona la cantidad de puntos según el cofre escogido anteriormente
+        else:
+            # Si no hay uno valido entonces se define como del tipo siguiente:
+            self.tipoJAMG = "Maldito"
+            self.puntosJAMG = -20
+        
+    def obtener_detalles(self):
+
+        """
+        Método utilizado para obtener los datos detallados
+        del cofre seleccionado
+        """
+        return self.tipo, self.puntos
+
+# Clase contraseña
+class ContrasenaJAMG:
+
+    """
+    Docstring: esta clase esta diseñada para crear y validar 
+    las constraseñas según los criterios de evaluación
+    """
+    def __init__(self):
+        pass
+
+# clase Juego Cazador
+
+class JuegoCazadorJAMG:
+
+    """
+    Docstring: esta es la clase principal, la cual contiene toda
+    la lógica de ejecución del juego
+    """
+
+    def __init__(self):
+        pass
+
