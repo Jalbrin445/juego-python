5b344cf1c1689b9e0c301f2a3157b10f50aa120c
Jalbrin445 <mezaguzmanjuanalbrin@gmail.com>
2026-07-09 16:53:26 -0500

    feat: Renombrar método obtener_detalles y agregar lógica para generación y validación de contraseñas

--- juego.py
+++ juego.py
@@ -59,13 +59,13 @@ class CofreJAMG:
             self.tipoJAMG = "Maldito"
             self.puntosJAMG = -20
         
-    def obtener_detalles(self):
+    def obtener_detallesJAMG(self):
 
         """
         Método utilizado para obtener los datos detallados
         del cofre seleccionado
         """
-        return self.tipo, self.puntos
+        return self.tipoJAMG, self.puntosJAMG
 
 # Clase contraseña
 class ContrasenaJAMG:
@@ -74,9 +74,53 @@ class ContrasenaJAMG:
     Docstring: esta clase esta diseñada para crear y validar 
     las constraseñas según los criterios de evaluación
     """
-    def __init__(self):
-        pass
 
+    CARACTERES_ESPECIALES_JAMG = "¿¡?=)(/¨*+-%&$#!." # String de caracteres especiales
+    
+    def __init__(self, longitudJAMG):
+        
+        self.longitudJAMG = longitudJAMG # este atributo es para que el usuario pueda ingresar la longitud por linea de comandos
+        self.valor = ""
+    
+    def validar_requisitosJAMG(self, passwordJAMG):
+        """
+        Método utilizado para validar que los requisitos 
+        de la contraseña creada si se cumplan según los criterios
+        de aceptación
+        """
+        tiene_mayusculaJAMG = any(c.isupper() for c in passwordJAMG) # any devuelve True si por lo menos uno de los valores es una mayuscula según el método .isupper()
+        tiene_minusculaJAMG = any(c.islower() for c in passwordJAMG)
+        tiene_numeroJAMG = any(c.isdigit() for c in passwordJAMG)
+        tiene_caracter_especialJAMG = any(c in self.CARACTERES_ESPECIALES_JAMG for c in passwordJAMG)
+        sin_repetidosJAMG = len(passwordJAMG) == len(set(passwordJAMG))
+
+        return tiene_mayusculaJAMG and tiene_minusculaJAMG and tiene_numeroJAMG and tiene_caracter_especialJAMG and sin_repetidosJAMG # devuelve un valor True o False
+    
+    def generar_contrasenaJAMG(self):
+        """
+        Este es el método utilizado para generar las contraseñas de forma
+        completamente aleatoria, teniendo en cuenta que debe pasar la validación
+        estricta, es decir, el método validar_requisitoJAMG
+        """
+
+        todos_los_caracteresJAMG = string.ascii_letters + string.digits + self.CARACTERES_ESPECIALES_JAMG
+
+        #Luego se convierte los caracteres anteriores a una lista para evaluar de forma más sencilla todo
+
+        lista_disponible = list(set(todos_los_caracteresJAMG))
+
+        if self.longitudJAMG > len(lista_disponible):
+            # Esto es para añadir más restricción o validación estricta, ya que no tiene sentido que la longitud de la contraseña sea mayor a la cantidad total de caracteres disponibles
+            raise LongitudInvalidaErrorJAMG("Longitud excesiva para generar caracteres sin repetir")
+        
+        caracteres_elegidosJAMG = random.sample(lista_disponible, self.longitudJAMG) # Con este método se pueden generar las contraseñas a partir de una lista de valores y una longitud, dando como resultado una contraseña sin caracteres repetidos
+        self.valor ="".join(caracteres_elegidosJAMG)
+
+        if not self.validar_requisitosJAMG(self.valor):
+            # Esta es la validación estricta obligatoria
+            raise ContrasenaIncorrectaErrorJAMG 
+        
+        return self.valor
 # clase Juego Cazador
 
 class JuegoCazadorJAMG:
